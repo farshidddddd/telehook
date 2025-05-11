72c0493cb95da2a5145c7f4ae5127325d867b2cf
farshidddddd <fr.robati250@gmail.com>
2025-05-11 13:50:09 +0330

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json());
+
+// توکن شما:
+const TOKEN = "7671033714:AAGezlzZD2uIU4Tm-xZwbuizQZahMyOVbUc";
+const TELEGRAM_API = https://api.telegram.org/bot${TOKEN};
+const WEBHOOK_PATH = /webhook/${TOKEN};
+const PORT = process.env.PORT || 3000;
+
+app.post(WEBHOOK_PATH, async (req, res) => {
+  const message = req.body.message;
+  if (message && message.text) {
+    const chatId = message.chat.id;
+    const text = message.text;
+
+    // ارسال پاسخ
+    await axios.post(${TELEGRAM_API}/sendMessage, {
+      chat_id: chatId,
+      text: شما گفتید: ${text}
+    });
+  }
+  res.sendStatus(200);
+});
+
+app.get("/", (req, res) => {
+  res.send("ربات تلگرام فعال است!");
+});
+
+app.listen(PORT, () => {
+  console.log(Server is running on port ${PORT});
+});
