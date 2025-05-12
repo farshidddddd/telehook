808df987e2793a43d04f765d3bccbf32f0e8b09c
farshidddddd <fr.robati250@gmail.com>
2025-05-12 03:57:00 +0330

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.json());
 
 // توکن ربات شما
 const TOKEN = "7671033714:AAGezlzZD2uIU4Tm-xZwbuizQZahMyOVbUc";
-const TELEGRAM_API = https://api.telegram.org/bot${TOKEN};
+const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`;
 const WEBHOOK_PATH = /webhook/${TOKEN};
 const PORT = process.env.PORT || 3000;
 
