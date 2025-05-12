fbfec84989f6ac9ad561560d5d34fdf807f421e6
farshidddddd <fr.robati250@gmail.com>
2025-05-12 15:42:58 +0330

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ bot.setWebHook(`${url}/bot${token}`);  // تنظیم Webhook
 const connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
-  user: root',
+  user: 'root',
   password: '',
   database: '1111',
   ssl: {
